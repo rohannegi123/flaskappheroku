25059859c91745923f9b9cb6157369566c860efc
rohannegi123 <84585663+rohannegi123@users.noreply.github.com>
2021-07-02 21:21:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 app= Flask(__name__)
 @app.route('/', methods = ['Get', 'Post'])
 def wow():
-    return "<h1>Abhijeet future tiktoker</h1>"
+    return "<h1>this is flask</h1>"
 
 if __name__ == "__main__":
     app.run()
